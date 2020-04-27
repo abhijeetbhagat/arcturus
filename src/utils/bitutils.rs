d562d5567df76a5cfac0f31933c7b14bfa114693
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-04-28 00:12:40 +0530

    Just use the bytes to integral type converters

--- src/utils/bitutils.rs
+++ src/utils/bitutils.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use std::slice::Iter;
 
 struct BitStream<'a> {
@@ -10,21 +11,31 @@ pub fn read_u8(slice: &mut Iter<u8>) -> u8 {
 }
 
 pub fn read_u16(slice: &mut Iter<u8>) -> u16 {
-    let first = *slice.next().unwrap();
-    let second = *slice.next().unwrap();
-    (0xffff & first as u16) << 8 | second as u16
+    let result = u16::from_be_bytes(slice.as_slice().get(0..2).unwrap().try_into().unwrap());
+    slice.next();
+    slice.next();
+    result
 }
 
 pub fn read_u32(slice: &mut Iter<u8>) -> u32 {
-    let first = *slice.next().unwrap();
-    let second = *slice.next().unwrap();
-    let third = *slice.next().unwrap();
-    let fourth = *slice.next().unwrap();
-    (0xffffffff & first as u32) << 24 | (second as u32) << 16 | (third as u32) << 8 | fourth as u32
+    let result = u32::from_be_bytes(slice.as_slice().get(0..4).unwrap().try_into().unwrap());
+    slice.next();
+    slice.next();
+    slice.next();
+    slice.next();
+    result
 }
 
 pub fn read_nbytes<'a>(slice: &'a mut Iter<u8>, n: usize) -> &'a [u8] {
-    slice.as_slice().get(0..n).unwrap()
+    let window = slice.as_slice().get(0..n).unwrap();
+    for i in 0..n {
+        slice.next();
+    }
+    window
+}
+
+pub fn to_txn_id(bytes: &[u8]) -> u128 {
+    u128::from_be_bytes(bytes.try_into().unwrap())
 }
 /*pub fn read_n(slice: &[u8], n: u32) -> &[u8] {
     slice.get()
