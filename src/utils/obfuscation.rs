2930002f139b26318171f90152cc43d288d58dad
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-04-29 18:17:15 +0530

    Split utility functions for obfuscation

--- src/utils/obfuscation.rs
+++ src/utils/obfuscation.rs
@@ -10,14 +10,17 @@ use async_std::net::{SocketAddrV4, SocketAddrV6};
     converting the result to network byte order.
 */
 
-pub fn obfuscate_v4(sock_addr: &SocketAddrV4, magic: u32) -> (u16, u32) {
-    let first_16bits = ((magic & 0xffff0000) >> 16) as u16;
+pub fn obfuscate_v4_ip_port(sock_addr: &SocketAddrV4, magic: u32) -> (u16, u32) {
     (
-        sock_addr.port() ^ first_16bits,
+        obfuscate_port(sock_addr.port(), magic),
         //deref because compiler doesn't understand
         //conversion from &IpV4Addr to u32
-        u32::from(*sock_addr.ip()) ^ magic,
+        obfuscate_v4_ip(u32::from(*sock_addr.ip()), magic),
     )
+}
+
+pub fn obfuscate_port(port: u16, magic: u32) -> u16 {
+    _xor_u16_and_u32(port, magic)
 }
 
 /*  If the IP address family is IPv6, X-Address is computed by taking the mapped IP address
@@ -25,6 +28,39 @@ pub fn obfuscate_v4(sock_addr: &SocketAddrV4, magic: u32) -> (u16, u32) {
     cookie and the 96-bit transaction ID, and converting the result to
     network byte order.
 */
-pub fn obfuscate_v6(sock_addr: &SocketAddrV6, magic: u32, txn_id: u128) -> (u16, u128) {
-    unimplemented!();
+pub fn obfuscate_v6_ip_port(sock_addr: &SocketAddrV6, magic: u32, txn_id: u128) -> (u16, u128) {
+    (
+        obfuscate_port(sock_addr.port(), magic),
+        obfuscate_v6_ip(u128::from(*sock_addr.ip()), magic, txn_id),
+    )
+}
+
+pub fn unobfuscate_port(port: u16, magic: u32) -> u16 {
+    _xor_u16_and_u32(port, magic)
+}
+
+pub fn obfuscate_v4_ip(ip: u32, magic: u32) -> u32 {
+    _xor_u32(ip, magic)
+}
+pub fn unobfuscate_v4_ip(ip: u32, magic: u32) -> u32 {
+    _xor_u32(ip, magic)
+}
+
+pub fn unobfuscate_v6_ip(ip: u128, magic: u32, txn_id: u128) -> u128 {
+    _xor_u128(ip, magic, txn_id)
+}
+pub fn obfuscate_v6_ip(ip: u128, magic: u32, txn_id: u128) -> u128 {
+    _xor_u128(ip, magic, txn_id)
+}
+
+fn _xor_u32(op1: u32, op2: u32) -> u32 {
+    op1 ^ op2
+}
+fn _xor_u16_and_u32(op1: u16, op2: u32) -> u16 {
+    let first_16bits = ((op2 & 0xffff0000) >> 16) as u16;
+    op1 ^ first_16bits
+}
+fn _xor_u128(op1: u128, op2: u32, op3: u128) -> u128 {
+    let operand = (op2 as u128) << 96 | op3;
+    op1 ^ operand
 }
