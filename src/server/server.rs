21bd9a7cf71aee0361961174f232f605e3919476
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-05-01 17:08:38 +0530

    Refactor - use the parse method for TCP mode too

--- src/server/server.rs
+++ src/server/server.rs
@@ -16,7 +16,7 @@ pub struct StunServer {}
 impl StunServer {
     pub async fn start(addr: impl ToSocketAddrs) -> Result<()> {
         let listener = TcpListener::bind(addr).await?;
-        println!("Started shining at {:?}", listener.local_addr()?);
+        println!("Started shining using TCP at {:?}", listener.local_addr()?);
         let mut incoming_stream = listener.incoming();
         while let Some(stream) = incoming_stream.next().await {
             let stream = stream?;
@@ -36,68 +36,12 @@ impl StunServer {
             //Check if this is a STUN request since other protocols
             //can be multiplexed according to https://tools.ietf.org/html/rfc5389#section-8
             if size > 0 {
-                if let Some(stun_message) = StunMessage::from_raw(&buf[0..size]) {
-                    let response = match stun_message.header.msg_type.0 {
-                        Class::Request => {
-                            match stun_message.header.msg_type.1 {
-                                Method::BindingRequest => {
-                                    //This is a binding request
-                                    let response = match source_transport_sock_addr {
-                                        SocketAddr::V4(v4_sock_addr) => {
-                                            let (xor_port, xor_mapped_addr) =
-                                                obfuscation::obfuscate_v4_ip_port(
-                                                    &v4_sock_addr,
-                                                    stun_message.header.magic,
-                                                );
-                                            let header = StunMessageHeader::new(
-                                                Type(Class::Success, Method::BindingResponse),
-                                                12,
-                                                stun_message.header.txn_id,
-                                            );
-                                            let attribute = XorMappedAddress::new(
-                                                1,
-                                                xor_port,
-                                                Left(xor_mapped_addr),
-                                            )
-                                            .as_raw();
-                                            StunMessage::new(header, Some(attribute))
-                                        }
-
-                                        SocketAddr::V6(v6_sock_addr) => {
-                                            let (xor_port, xor_mapped_addr) =
-                                                obfuscation::obfuscate_v6_ip_port(
-                                                    &v6_sock_addr,
-                                                    stun_message.header.magic,
-                                                    stun_message.header.txn_id,
-                                                );
-                                            let header = StunMessageHeader::new(
-                                                Type(Class::Success, Method::BindingResponse),
-                                                24,
-                                                stun_message.header.txn_id,
-                                            );
-                                            let attribute = XorMappedAddress::new(
-                                                2,
-                                                xor_port,
-                                                Right(xor_mapped_addr),
-                                            )
-                                            .as_raw();
-                                            StunMessage::new(header, Some(attribute))
-                                        }
-                                    };
-
-                                    Some(response)
-                                }
-                                _ => None,
-                            }
-                        }
-                        _ => None,
-                    };
-                    if let Some(response) = response {
-                        println!("Sending STUN response");
-                        writer.write(&response.as_raw()).await?;
-                        writer.flush();
-                        buf.clear();
-                    }
+                if let Some(stun_message) =
+                    StunServer::parse(&buf[..size], source_transport_sock_addr)
+                {
+                    writer.write(&stun_message.as_raw()).await?;
+                    writer.flush();
+                    buf.clear();
                 } else {
                     continue;
                 }
@@ -108,11 +52,14 @@ impl StunServer {
 
     pub async fn start_udp(addr: impl ToSocketAddrs) -> Result<()> {
         let socket = UdpSocket::bind(addr).await?;
+        println!("Started shining using UDP at {:?}", socket.local_addr()?);
         let mut buf = vec![0; 1024];
         loop {
             let (size, peer) = socket.recv_from(&mut buf).await?;
-            if let Some(stun_message) = StunServer::parse(&buf[..size], peer) {
-                socket.send(&buf[..size]).await?;
+            if size > 0 {
+                if let Some(stun_message) = StunServer::parse(&buf[..size], peer) {
+                    socket.send(&stun_message.as_raw()).await?;
+                }
             }
         }
         Ok(())
