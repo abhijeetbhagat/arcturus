561b897887fbebc370e569979d20af6a9e2f78cb
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2020-05-28 10:16:20 +0530

    Use Into<usize> trait bound for param in read_nbytes

--- src/utils/bitutils.rs
+++ src/utils/bitutils.rs
@@ -19,51 +19,43 @@ impl<'a> BitStream<'a> {
     pub fn read_u8(&self) -> Option<u8> {
         let result = self.data.get(*self.current_pos.borrow())?;
         *self.current_pos.borrow_mut() += 1;
-        //*pos += 1;
         Some(*result)
     }
 
     pub fn read_u16(&self) -> Option<u16> {
-        //println!("{}"*self.current_pos.borrow())
         let result = u16::from_be_bytes({
             let pos = self.get_pos();
             self.data.get(pos..pos + 2)?.try_into().unwrap()
         });
         *self.current_pos.borrow_mut() += 2;
-        //*pos += 1;
         Some(result)
     }
 
     pub fn read_u32(&self) -> Option<u32> {
-        //println!("{}"*self.current_pos.borrow())
         let result = u32::from_be_bytes({
             let pos = self.get_pos();
             self.data.get(pos..pos + 4)?.try_into().unwrap()
         });
         *self.current_pos.borrow_mut() += 4;
-        //*pos += 1;
         Some(result)
     }
 
     pub fn read_u128(&self) -> Option<u128> {
-        //println!("{}"*self.current_pos.borrow())
         let result = u128::from_be_bytes({
             let pos = self.get_pos();
             self.data.get(pos..pos + 16)?.try_into().unwrap()
         });
         *self.current_pos.borrow_mut() += 16;
-        //*pos += 1;
         Some(result)
     }
 
-    pub fn read_nbytes(&self, n: u32) -> Option<&'a [u8]> {
-        //println!("{}"*self.current_pos.borrow())
+    pub fn read_nbytes(&self, n: impl Into<usize>) -> Option<&'a [u8]> {
+        let n = n.into();
         let result = {
             let pos = self.get_pos();
-            self.data.get(pos..pos + n as usize)?
+            self.data.get(pos..pos + n)?
         };
-        *self.current_pos.borrow_mut() += n as usize;
-        //*pos += 1;
+        *self.current_pos.borrow_mut() += n;
         Some(result)
     }
 
@@ -111,9 +103,6 @@ pub fn read_nbytes<'a>(slice: &'a mut Iter<u8>, n: usize) -> &'a [u8] {
 pub fn to_txn_id(bytes: &[u8]) -> u128 {
     u128::from_be_bytes(bytes.try_into().unwrap())
 }
-/*pub fn read_n(slice: &[u8], n: u32) -> &[u8] {
-    slice.get()
-}*/
 
 #[cfg(test)]
 mod test {
@@ -150,6 +139,6 @@ mod test {
         assert_eq!(reader.read_u16(), Some(0x102));
         assert_eq!(reader.read_u16(), Some(0x304));
         assert_eq!(reader.read_u32(), Some(0x5_060_708));
-        assert_eq!(Some(&[9, 10][..]), reader.read_nbytes(2));
+        assert_eq!(Some(&[9, 10][..]), reader.read_nbytes(2u8));
     }
 }
